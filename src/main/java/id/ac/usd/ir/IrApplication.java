02e0e46829b47ed3f1f37b23870e8558715ce874
Yosafat Willy Christian <willychristt@gmail.com>
2022-10-24 22:38:52 +0700

    update

--- src/main/java/id/ac/usd/ir/IrApplication.java
+++ src/main/java/id/ac/usd/ir/IrApplication.java
@@ -2,12 +2,18 @@ package id.ac.usd.ir;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class IrApplication {
+public class IrApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IrApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(IrApplication.class);
+	}
 }
